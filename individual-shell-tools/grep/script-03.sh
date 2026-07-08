e8c853b5bc302f765c66b8d017f1de9086c8fdaa
sarawone <sarawone.poudal@gmail.com>
2026-07-08 14:44:00 +0100

    grep_exercise

--- individual-shell-tools/grep/script-03.sh
+++ individual-shell-tools/grep/script-03.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # TODO: Write a command to output the number of lines in dialogue.txt that contain the word Doctor (regardless of case).
 # The output should be exactly the number 9.
+
+cd individual-shell-tools/grep
+grep -ic "Doctor" dialogue.txt
--- individual-shell-tools/grep/script-04.sh
+++ individual-shell-tools/grep/script-04.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # TODO: Write a command to output every line in dialogue.txt that does not contain the word "Hello" (regardless of case).
 # The output should contain 10 lines.
+
+cd individual-shell-tools/grep
+grep -iv "Hello" dialogue.txt
