3c27d0c266f0695a798befa014b0285893d936fb
sarawone <sarawone.poudal@gmail.com>
2026-07-08 12:26:55 +0100

    ls_exercise

--- individual-shell-tools/ls/script-03.sh
+++ individual-shell-tools/ls/script-03.sh
@@ -3,5 +3,9 @@
 set -euo pipefail
 
 # TODO: Write a command which _recursively_ lists all of the files and folders in this directory _and_ all of the files inside those folders.
+cd individual-shell-tools/ 
+cd ls
+ls -R
+
 # The output should be a list of names including: child-directory, script-01.sh, helper-1.txt (and more).
 # The formatting of the output doesn't matter.
--- individual-shell-tools/ls/script-04.sh
+++ individual-shell-tools/ls/script-04.sh
@@ -15,9 +15,12 @@ echo "First exercise (sorted newest to oldest):"
 
 # TODO: Write a command which lists the files in the child-directory directory, one per line, sorted so that the most recently modified file is first.
 # The output should be a list of names in this order, one per line: helper-3.txt, helper-1.txt, helper-2.txt.
-
+cd individual-shell-tools/ls/child-directory
+ls -lt
 
 echo "Second exercise (sorted oldest to newest):"
 
 # TODO: Write a command which does the same as above, but sorted in the opposite order (oldest first).
 # The output should be a list of names in this order, one per line: helper-2.txt, helper-1.txt, helper-3.txt.
+cd individual-shell-tools/ls/child-directory
+ls -ltr
