9a3cee04e49b59131388dc45320fff886e4c4ff3
sarawone <sarawone.poudal@gmail.com>
2026-07-08 12:59:47 +0100

    wc_exercise

--- individual-shell-tools/wc/script-03.sh
+++ individual-shell-tools/wc/script-03.sh
@@ -8,3 +8,6 @@ set -euo pipefail
 #        1       7      39 ../helper-files/helper-2.txt
 #        3      19      92 ../helper-files/helper-3.txt
 #        5      30     151 total
+
+cd individual-shell-tools/helper-files
+wc helper-1.txt helper-2.txt helper-3.txt 
