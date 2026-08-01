660bf2077c88a63a6fde937b451210233538a713
sarawone <sarawone.poudal@gmail.com>
2026-08-01 11:04:06 +0100

    exercise1.1

---
+++ .github/Perp-Exercise/exercise1.1.py
@@ -0,0 +1,8 @@
+def double(value):
+    return value * 2 
+#Question
+#Predict what double("22") will do. Then run the code and check. Did it do what you expected? Why did it return the value it did?
+
+#Answer
+# it will return "2222"
+#In python, the string*number shows the number of times of the string.
