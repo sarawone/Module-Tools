8636c311b3e1c703864190bca4a62c42111f5352
sarawone <sarawone.poudal@gmail.com>
2026-08-01 11:09:45 +0100

    exercise_1.2

---
+++ .github/Perp-Exercise/exercise1.2.py
@@ -0,0 +1,12 @@
+def double(number):
+    return number * 3
+
+print(double(10))
+
+#Question
+#Read the above code and write down what the bug is. How would you fix it?
+
+#Answer
+
+#The code is correct but the function name as double but the code is calculated for Triple .
+#I can fix with two method. 1. change to function name to triple 2. change return number *3 to number *2
